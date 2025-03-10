7bc1ba2980d51748967b691910a5fcce2d68e2c9
Luiz Barros <lck.barros@gmail.com>
2025-03-09 21:31:15 -0300

    Definidas variáveis de chamada do elemento Canvas, de contexto 2D, de altura e largura do canvas, assim como tamanho dos blocos e velocidade do game

--- src/script.js
+++ src/script.js
@@ -1,3 +1,13 @@
 window.onload = function() { 
     document.getElementById("ano").innerHTML = new Date().getFullYear(); 
   }
+
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.height;
+
+const blockSize = 20;
+
+const gameSpeed = 100;
