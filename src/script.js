5a4cea8ae3d006752e3f54b1bec848b02618a7d2
Luiz Barros <lck.barros@gmail.com>
2025-03-10 23:08:46 -0300

    Finalizado a definição das variáveis globais

--- src/script.js
+++ src/script.js
@@ -4,10 +4,23 @@ window.onload = function() {
 
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
-
 const canvasWidth = canvas.width;
 const canvasHeight = canvas.height;
 
 const blockSize = 20;
-
 const gameSpeed = 100;
+
+let snake = [
+  {x: 200, y: 200},
+  {x: 180, y: 200},
+  {x: 160, y: 200},
+];
+
+let direction = "right";
+
+let food = {
+  x: Math.floor (Math.random() * (canvasWidth / blockSize)) * blockSize,
+  y: Math.floor (Math.random() * (canvasHeight / blockSize)) * blockSize,
+}
+
+let score = 0;
