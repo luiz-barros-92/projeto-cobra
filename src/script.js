2e5563fbf14747603c3704a3e4a1329cb215062f
Luiz Barros <lck.barros@gmail.com>
2025-03-11 22:57:09 -0300

    Implementada função para desenhar a cobra, assim como definida a cor e redefinido tamanho do corpo

--- src/script.js
+++ src/script.js
@@ -4,23 +4,35 @@ window.onload = function() {
 
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
+
 const canvasWidth = canvas.width;
 const canvasHeight = canvas.height;
 
-const blockSize = 20;
+const blockSize = 10;
 const gameSpeed = 100;
 
 let snake = [
   {x: 200, y: 200},
+  {x: 190, y: 200},
   {x: 180, y: 200},
-  {x: 160, y: 200},
 ];
 
 let direction = "right";
 
 let food = {
   x: Math.floor (Math.random() * (canvasWidth / blockSize)) * blockSize,
-  y: Math.floor (Math.random() * (canvasHeight / blockSize)) * blockSize,
+  y: Math.floor (Math.random() * (canvasHeight / blockSize)) * blockSize
 }
 
 let score = 0;
+
+function drawSnake() {
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+  snake.forEach((block, index) => {
+    ctx.fillStyle = "#32CD32";
+    ctx.fillRect(block.x, block.y, blockSize, blockSize);    
+  });  
+}
+
+drawSnake();
